839eee572d97ba327236f8425b6bee97934cbef3
Jordan Fred <jcfred88@gmail.com>
2017-05-08 16:06:57 -0600

    brance commit pre merge

--- app.js
+++ app.js
@@ -39,35 +39,36 @@ function allData(match, names){
       wins: 0,
       loses: 0
     }
-    //for each match
-    for (var m = 0; m < match.length; m++) {
-      if(players[p].id === match[m].winner_id){
-        players[p].total += 1
-        players[p].wins += 1
-      }
-      if(players[p].id === match[m].loser_id){
-        players[p].total += 1
-        players[p].loses += 1
-      }
-      console.log(players[match[m].winner_id -1].name);
-      console.log(players[match[m].loser_id -1].name);
-      let tempWinner = players[match[m].winner_id -1].name
-      let tempLoser = players[match[m].loser_id -1].name
+  }
 
+  //for each player/each match
+  for (var q = 0; q < names.length; q++) {
+    for (var m = 0; m < match.length; m++) {
+      if(players[q]['id'] == match[m]['winner_id']){
+        players[q].total += 1
+        players[q].wins += 1
+      }
+      if(players[q].id === match[m].loser_id){
+        players[q].total += 1
+        players[q].loses += 1
+      }
+
+      let winnerID = match[m].winner_id -1
+      let LoserID = match[m].loser_id -1
       games[m] = {
-        winner: tempWinner,
-        loser: tempLoser,
+        winner: players[winnerID]["name"],
+        loser: players[LoserID]["name"],
         date: match[m].date
       }
 
     }
   }
+  let returnObj = {
+    games: games,
+    players: players
+  }
 
-  let returnArr = []
-  returnArr[0] = games
-  returnArr[1] = players
-  console.log(returnArr);
-  return returnArr
+  return returnObj
 
 }
 
